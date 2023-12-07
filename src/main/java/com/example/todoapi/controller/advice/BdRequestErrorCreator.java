ddd994b27525fd7c3ac6a3b476fd9c0426d1edfa
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2023-12-08 05:35:10 +0900

    81.400 BadRequest(フィールド欠損)：リファクタリング

--- src/main/java/com/example/todoapi/controller/advice/BdRequestErrorCreator.java
+++ src/main/java/com/example/todoapi/controller/advice/BdRequestErrorCreator.java
@@ -2,25 +2,34 @@ package com.example.todoapi.controller.advice;
 
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.InvalidParam;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class BdRequestErrorCreator {
     public static BadRequestError from(MethodArgumentNotValidException ex) {
-        var invalidParamList = ex.getFieldErrors()
-                .stream()
-                .map(fieldError -> {
-                    var invalidParam = new InvalidParam();
-                    invalidParam.setName(fieldError.getField());
-                    invalidParam.setReason(fieldError.getDefaultMessage());
-                    return invalidParam;
-                })
-                .collect(Collectors.toList());
+        var invalidParamList = createInvaliParamList(ex);
 
         var error = new BadRequestError();
         error.setInvalidParams(invalidParamList);
 
         return error;
+    }
+
+    private static List<InvalidParam> createInvaliParamList(MethodArgumentNotValidException ex) {
+        return ex.getFieldErrors()
+                .stream()
+                .map(fieldError -> createInvalidParam(fieldError))
+                .collect(Collectors.toList());
+    }
+
+    private static InvalidParam createInvalidParam
+            (FieldError fieldError) {
+        var invalidParam = new InvalidParam();
+        invalidParam.setName(fieldError.getField());
+        invalidParam.setReason(fieldError.getDefaultMessage());
+        return invalidParam;
     }
 }
